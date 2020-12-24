968ecb1a35663aaf85e3a7e60e32590e67fe2235
Ryan Hartlage <ryanplusplus@gmail.com>
2020-12-24 06:35:07 -0500

    Use tiny_heartbeat, enable better warnings

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,17 @@
 
 extern "C" {
 #include <stddef.h>
-#include <tiny_digital_output.h>
-#include <tiny_timer.h>
-#include <tiny_time_source.h>
+#include "tiny_timer.h"
+#include "tiny_time_source.h"
+#include "tiny_heartbeat.h"
 }
 
-enum {
-  blink_period = 1000
-};
-
-static tiny_digital_output_t led;
 static tiny_timer_group_t timer_group;
-static tiny_timer_t blink_timer;
-
-static void blink(tiny_timer_group_t* timer_group, void* context)
-{
-  (void)timer_group;
-  static bool state;
-  state = !state;
-  tiny_digital_output_write(&led.interface, state);
-}
 
 void setup()
 {
-  tiny_digital_output_init(&led, LED_BUILTIN);
   tiny_timer_group_init(&timer_group, tiny_time_source_init());
-
-  tiny_timer_start_periodic(&timer_group, &blink_timer, blink_period / 2, blink, NULL);
+  tiny_heartbeat_init(&timer_group, 1000);
 }
 
 void loop()
